55e2b4332b377f25b0519ed123a68ad572781d83
Mary Warrington <student@epicodus.com>
2016-03-18 08:50:46 -0700

    Add initial repo-interface logic

--- js/repository-interface.js
+++ js/repository-interface.js
@@ -3,8 +3,17 @@ var apiKey = require('./../.env').apiKey;
 var tempConvertToF = require('./../js/scripts.js');
 
 $(document).ready(function() {
+  $('.show-repos').hide();
+
+  $('#repobutton').click(function() {
+    var username = $('#username').val();
+    $('#username').val("");
+    $.get('https://api.github.com/users/daneden?access_token=' + apiKey).then(function(response){
+      console.log(response);
+    }).fail(function(error){
+      console.log(error.responseJSON.message);
+    });
 
 
-
-
+  })
 });
